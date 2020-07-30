7ce33a3ccb8a9b1746ef5df7d8b9a0e487d2a33d
YuShun <2305808176@qq.com>
2020-07-30 20:28:58 +0800

    可以通过T2进行基本的通讯
    完善uart传输

--- MPU6050-Test/main.c
+++ MPU6050-Test/main.c
@@ -1,6 +1,157 @@
 #include "15w.h"
 
+
+// 声明元器件管脚
+
+#define Vcc P20		//电源正极
+#define Gnd P21		//电源负极
+#define Scl P22		//I2C时钟线
+#define Sda P23		//I2C数据线
+#define Xda P24		//外接I2C数据线
+#define Xcl P25		//外接I2C时钟线
+#define Ad0 P26		//I2C地址最低位
+#define Int P27		//中断信号输出
+
+// 声明串口传输设置
+
+#define FOSC 24000000L		//系统频率
+#define BAUD 115200 		//波特率
+#define NONE_PARITY     0       //无校验
+#define ODD_PARITY      1       //奇校验
+#define EVEN_PARITY     2       //偶校验
+#define MARK_PARITY     3       //标记校验
+#define SPACE_PARITY    4       //空白校验
+#define PARITYBIT NONE_PARITY   //定义校验位
+#define S1_S0 0x40              //P_SW1.6
+#define S1_S1 0x80              //P_SW1.7
+
+// 声明mpu6050内部地址
+#define	SMPLRT_DIV		0x19	//陀螺仪采样率，典型值：0x07(125Hz)
+#define	CONFIG			0x1A	//低通滤波频率，典型值：0x06(5Hz)
+#define	GYRO_CONFIG		0x1B	//陀螺仪自检及测量范围，典型值：0x18(不自检，2000deg/s)
+#define	ACCEL_CONFIG	0x1C	//加速计自检、测量范围及高通滤波频率，典型值：0x01(不自检，2G，5Hz)
+#define	ACCEL_XOUT_H	0x3B
+#define	ACCEL_XOUT_L	0x3C
+#define	ACCEL_YOUT_H	0x3D
+#define	ACCEL_YOUT_L	0x3E
+#define	ACCEL_ZOUT_H	0x3F
+#define	ACCEL_ZOUT_L	0x40
+#define	TEMP_OUT_H		0x41
+#define	TEMP_OUT_L		0x42
+#define	GYRO_XOUT_H		0x43
+#define	GYRO_XOUT_L		0x44	
+#define	GYRO_YOUT_H		0x45
+#define	GYRO_YOUT_L		0x46
+#define	GYRO_ZOUT_H		0x47
+#define	GYRO_ZOUT_L		0x48
+#define	PWR_MGMT_1		0x6B	//电源管理，典型值：0x00(正常启用)
+#define	WHO_AM_I		0x75	//IIC地址寄存器(默认数值0x68，只读)
+#define	SlaveAddress	0xD0	//IIC写入时的地址字节数据，+1为读取
+
+// 变量声明
+
+bit busy;
+
+// 提前声明函数
+
+void mpu6050_init();
+void uart_init();
+void SendData(unsigned char dat);
+void SendString(char *s);
+
 int main(void)
 {
+	mpu6050_init();
+	uart_init();
+	T2L = (65536 - (FOSC/4/BAUD));   //设置波特率重装值
+    T2H = (65536 - (FOSC/4/BAUD))>>8;
+    AUXR = 0x14;                //T2为1T模式, 并启动定时器2
+    AUXR |= 0x01;               //选择定时器2为串口1的波特率发生器
+    ES = 1;                     //使能串口1中断
+    EA = 1;
+
+	SendString("STC15F2K60S2\r\nUart Test !\r\n");
+	while (1)
+	{
+		
+	}
+	
+
 	return 0;
+}
+
+/*mpu6050初始化函数*/
+void mpu6050_init ()
+{
+	// 配置IO输出模式
+	P2M0 = 0x01;
+	P2M1 = 0x00;
+	//配置电源电平
+	Vcc = 1;
+	Gnd = 0;
+}
+
+void uart_init ()
+{
+	ACC = P_SW1;
+    ACC &= ~(S1_S0 | S1_S1);    //S1_S0=0 S1_S1=0
+    P_SW1 = ACC;                //(P3.0/RxD, P3.1/TxD)
+	#if (PARITYBIT == NONE_PARITY)
+    SCON = 0x50;                //8位可变波特率
+	#elif (PARITYBIT == ODD_PARITY) || (PARITYBIT == EVEN_PARITY) || (PARITYBIT == MARK_PARITY)
+    SCON = 0xda;                //9位可变波特率,校验位初始为1
+	#elif (PARITYBIT == SPACE_PARITY)
+    SCON = 0xd2;                //9位可变波特率,校验位初始为0
+	#endif
+}
+
+/*串口数据发送*/
+void SendData(unsigned char dat)
+{
+    while (busy);               //等待前面的数据发送完成
+    ACC = dat;                  //获取校验位P (PSW.0)
+    if (P)                      //根据P来设置校验位
+    {
+		#if (PARITYBIT == ODD_PARITY)
+			TB8 = 0;                //设置校验位为0
+		#elif (PARITYBIT == EVEN_PARITY)
+			TB8 = 1;                //设置校验位为1
+		#endif
+    }
+    else
+    {
+		#if (PARITYBIT == ODD_PARITY)
+    	    TB8 = 1;                //设置校验位为1
+		#elif (PARITYBIT == EVEN_PARITY)
+    	    TB8 = 0;                //设置校验位为0
+		#endif
+    }
+    busy = 1;
+    SBUF = ACC;                 //写数据到UART数据寄存器
+}
+
+/*发送字符串*/
+void SendString(char *s)
+{
+    while (*s)                  //检测字符串结束标志
+    {
+        SendData(*s++);         //发送当前字符
+    }
+}
+
+
+/*uart中断服务程序*/
+void Uart() interrupt 4
+{
+    if (RI)
+    {
+        RI = 0;                 //清除RI位
+        P0 = SBUF;              //P0显示串口数据
+        P54 = RB8;              //P2.2显示校验位
+    }
+    if (TI)
+    {
+        TI = 0;                 //清除TI位
+        busy = 0;               //清忙标志
+    }
 }
